623599d6c5a0616dcd827f05742e11cda0c36567
waqas2714 <waqasali00123@gmail.com>
2024-01-15 21:25:04 +0500

    Token verification middleware added

---
+++ middlewares/verifyUser.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const verifyUser = async (req, res, next)=>{
+    try {
+        const authorizationHeader = req.header("Authorization");
+    
+        if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+            throw new Error("Not Authorized. Please provide a valid Bearer token.");
+        }
+    
+        const accessToken = authorizationHeader.split(" ")[1];
+    
+        if (!accessToken) {
+            throw new Error("Not Authorized. Please log in properly.");
+        }
+    
+        const payload = jwt.verify(accessToken, process.env.JWT_SECRET);
+    
+        console.log(payload);
+    
+        req.user = payload;
+    
+        return next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            res.status(401).json({ error: 'Token has expired' });
+        } else {
+            res.status(401).json({ error: error.message });
+        }
+    }
+    
+}
+
+module.exports = verifyUser
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
 const app = express();
 
-app.use('/api/auth', authRoutes)
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 mongoose
