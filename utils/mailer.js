cc74d28e7d56ead34030b66630932d833d1c7cf9
waqas2714 <waqasali00123@gmail.com>
2024-04-17 23:43:24 +0500

    changed mail to gmail

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,15 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.office365.com', // Outlook SMTP server hostname
-  port: 587, // Port for secure TLS connection
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.USER_EMAIL, // Your Outlook email address
-    pass: process.env.USER_EMAIL_PASSWORD + "#" // Your Outlook email password
-    // user: "waqasali00123@gmail.com", // Your Outlook email address
-    // pass: "OutlookPassword1#" // Your Outlook email password
-  }
+  host: 'smtp.gmail.com', // Gmail SMTP server hostname
+            port: 587,
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.USER_EMAIL_PASSWORD
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
 });
 
 module.exports = { transporter };
