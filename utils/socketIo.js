32a3ae28549fd9f1030f67786dd24b943d5304b3
waqas2714 <waqasali00123@gmail.com>
2024-04-20 01:34:23 +0500

    leave-room functionality added in socketio

--- utils/socketIo.js
+++ utils/socketIo.js
@@ -4,6 +4,10 @@ const initializeSocketIo = (io) => {
       socket.join(listingId); 
     });
 
+    socket.on("leave-room", (listingId) => {
+      socket.leave(listingId); 
+    });
+
     socket.on("send-msg", (data) => {
       // Broadcast the message to all users in the room except the sender
       socket.broadcast.to(data.listingId).emit("msg-recieve", data);
